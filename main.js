dfe08069a0c1c981eecbf1072a78abd638531d1c
Rubylin Castillo <Rubylin.Castillo@rcastillo.2ldxhoqz4w0efi5ajh3u0k0m2g.bx.internal.cloudapp.net>
2019-02-28 21:33:07 +0000

    Refactored populate dropdown

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const urlCountries = 'https://xc-ajax-demo.herokuapp.com/api/countries/';
 getCountries();
 
+function populateCountryDropdown(countriesList){
+ console.log(JSON.stringify(countriesList));
+        var countrySelection = document.getElementById('countries');
+        for(let countryIndex = 0; countryIndex < countriesList.length; countryIndex++) {
+            var countryOption = document.createElement('OPTION');
+            countryOption.innerHTML =  countriesList[countryIndex].name;
+            countryOption.value = countriesList[countryIndex].code;
+            countrySelection.options.add(countryOption);
+        }
+}
+
+
 function getCountries() {
     const endpoint = `${urlCountries}`;
 
@@ -12,14 +24,7 @@ function getCountries() {
     }, networkError => {
         console.log(networkError.message)
     }).then(function(countriesList) {
-        console.log(JSON.stringify(countriesList));
-        var countrySelection = document.getElementById('countries');
-        for(let countryIndex = 0; countryIndex < countriesList.length; countryIndex++) {
-            var countryOption = document.createElement('OPTION');
-            countryOption.innerHTML =  countriesList[countryIndex].name;
-            countryOption.value = countriesList[countryIndex].code;
-            countrySelection.options.add(countryOption);
-        }
+        populateCountryDropdown(countriesList);
     })
 }
 
@@ -34,6 +39,23 @@ function buildStateURL() {
     return `https://xc-ajax-demo.herokuapp.com/api/countries/${getSelectedCountryCode()}/states/`;
 }
 
+function populateStateDropdown(statesList){
+    console.log(JSON.stringify(statesList));
+    var stateSelection = document.getElementById('states');
+    stateSelection.innerHTML = '';
+    alphabetizedStates = [statesList.length];
+    for(let stateIndex = 0; stateIndex < statesList.length; stateIndex++){
+        alphabetizedStates[stateIndex] = statesList[stateIndex].name;
+        alphabetizedStates[stateIndex].value = statesList[stateIndex].code;
+    }
+    alphabetizedStates.sort();
+    for(let stateIndex = 0; stateIndex < alphabetizedStates.length; stateIndex++) {
+        var stateOption = document.createElement('OPTION');
+        stateOption.innerHTML = alphabetizedStates[stateIndex];
+        stateSelection.options.add(stateOption);
+    }
+}
+
 function getStates() {
     fetch(buildStateURL()).then(response => {
         if(response.ok){
@@ -43,19 +65,6 @@ function getStates() {
     }, networkError => {
         console.log(networkError.message)
     }).then(function(statesList) {
-        console.log(JSON.stringify(statesList));
-        var stateSelection = document.getElementById('states');
-        stateSelection.innerHTML = '';
-        alphabetizedStates = [statesList.length];
-        for(let stateIndex = 0; stateIndex < statesList.length; stateIndex++){
-            alphabetizedStates[stateIndex] = statesList[stateIndex].name;
-            alphabetizedStates[stateIndex].value = statesList[stateIndex].code;
-        }
-        alphabetizedStates.sort();
-        for(let stateIndex = 0; stateIndex < alphabetizedStates.length; stateIndex++) {
-            var stateOption = document.createElement('OPTION');
-            stateOption.innerHTML = alphabetizedStates[stateIndex];
-            stateSelection.options.add(stateOption);
-        }
+        populateStateDropdown(statesList);
     })
 }
