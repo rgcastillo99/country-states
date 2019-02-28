a446d6e1bc73925b9504523e9857ac94a4562283
Rubylin Castillo <Rubylin.Castillo@rcastillo.2ldxhoqz4w0efi5ajh3u0k0m2g.bx.internal.cloudapp.net>
2019-02-28 20:50:05 +0000

    Initializing Git Countries/States Working

---
+++ main.js
@@ -0,0 +1,61 @@
+const urlCountries = 'https://xc-ajax-demo.herokuapp.com/api/countries/';
+getCountries();
+
+function getCountries() {
+    const endpoint = `${urlCountries}`;
+
+    fetch(endpoint).then(response => {
+        if(response.ok){
+            return response.json();
+        }
+        throw new Error('Request Failed.');
+    }, networkError => {
+        console.log(networkError.message)
+    }).then(function(countriesList) {
+        console.log(JSON.stringify(countriesList));
+        var countrySelection = document.getElementById('countries');
+        for(let countryIndex = 0; countryIndex < countriesList.length; countryIndex++) {
+            var countryOption = document.createElement('OPTION');
+            countryOption.innerHTML =  countriesList[countryIndex].name;
+            countryOption.value = countriesList[countryIndex].code;
+            countrySelection.options.add(countryOption);
+        }
+    })
+}
+
+function getSelectedCountryCode() {
+    var countriesDropdown = document.getElementById('countries');
+    console.log(countriesDropdown);
+    var selectedCountryValue = countriesDropdown.value;    
+    return selectedCountryValue;
+}
+
+function buildStateURL() {
+    return `https://xc-ajax-demo.herokuapp.com/api/countries/${getSelectedCountryCode()}/states/`;
+}
+
+function getStates() {
+    fetch(buildStateURL()).then(response => {
+        if(response.ok){
+            return response.json();
+        }
+        throw new Error('Request Failed.');
+    }, networkError => {
+        console.log(networkError.message)
+    }).then(function(statesList) {
+        console.log(JSON.stringify(statesList));
+        var stateSelection = document.getElementById('states');
+        stateSelection.innerHTML = '';
+        alphabetizedStates = [statesList.length];
+        for(let stateIndex = 0; stateIndex < statesList.length; stateIndex++){
+            alphabetizedStates[stateIndex] = statesList[stateIndex].name;
+            alphabetizedStates[stateIndex].value = statesList[stateIndex].code;
+        }
+        alphabetizedStates.sort();
+        for(let stateIndex = 0; stateIndex < alphabetizedStates.length; stateIndex++) {
+            var stateOption = document.createElement('OPTION');
+            stateOption.innerHTML = alphabetizedStates[stateIndex];
+            stateSelection.options.add(stateOption);
+        }
+    })
+}
